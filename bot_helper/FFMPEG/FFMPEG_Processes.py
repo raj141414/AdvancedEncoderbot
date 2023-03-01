d83896d54351333d805489e735912c78d99d6c78
sahilgit55 <sahilgit55@gmail.com>
2023-03-02 03:20:17 +0530

    test

--- bot_helper/FFMPEG/FFMPEG_Processes.py
+++ bot_helper/FFMPEG/FFMPEG_Processes.py
@@ -180,8 +180,12 @@ class FFMPEG:
                         process_status.update_process_message(f"🎞Generating Sample Video\n`{str(file_name)}`\n{process_status.get_task_details()}")
                         sample_name = f"{process_status.dir}/sample_{file_name}"
                         vstart_time, vend_time = await get_cut_duration(duration)
+                        if duration<180:
+                                vframes = '900'
+                        else:
+                                vframes = '1800'
                         # cmd_sample = ["ffmpeg", "-ss", str(vstart_time), "-to",  str(vend_time), "-i", f"{input_video}","-c", "copy", '-y', f"{sample_name}"]
-                        cmd_sample= ['ffmpeg', '-ss', f'{vstart_time}s', '-i', f"{input_video}", '-vframes', '1800', '-vsync', '1', '-async', '-1', '-acodec', 'copy', '-vcodec', 'copy', '-y', f"{sample_name}"]
+                        cmd_sample= ['ffmpeg', '-ss', f'{vstart_time}s', '-i', f"{input_video}", '-vframes', f'{vframes}', '-vsync', '1', '-async', '-1', '-acodec', 'copy', '-vcodec', 'copy', '-y', f"{sample_name}"]
                         sample_result = await run_process_command(cmd_sample)
                         if sample_result and exists(sample_name):
                                 try:
@@ -191,6 +195,9 @@ class FFMPEG:
                                 remove(sample_name)
                         else:
                                 await process_status.event.reply(f'❌Failed To Generate Sample Video')
+                else:
+                        if force_gen:
+                                await process_status.event.reply(f'❌Video Duration Must Be Greater Than 60 secs To Generate Sample')
         return
     ###############------Change_Metadata------###############
     async def change_metadata(process_status):
